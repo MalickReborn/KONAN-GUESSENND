68c9d0d52d7f3adc4351f0755a5c8e2f77fc8b8c
MalickReborn <malickguessennd@gmail.com>
2022-06-15 20:32:28 +0000

    add details Post

--- KONANGUESSENND/blog/models.py
+++ KONANGUESSENND/blog/models.py
@@ -1,3 +1,10 @@
+from datetime import date
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+class Post(models.Model):
+    article = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    created_date = models.DateTimeField
+    Published_date = models.DateTimeField
