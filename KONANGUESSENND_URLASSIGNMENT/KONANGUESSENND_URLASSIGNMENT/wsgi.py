3687dbb5556b3d5865c8ded5b403f6c17fbd67c5
MalickReborn <malickguessennd@gmail.com>
2022-06-16 22:31:20 +0000

    url assignment

--- KONANGUESSENND_URLASSIGNMENT/KONANGUESSENND_URLASSIGNMENT/wsgi.py
+++ KONANGUESSENND_URLASSIGNMENT/KONANGUESSENND_URLASSIGNMENT/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for KONANGUESSENND project.
+WSGI config for KONANGUESSENND_URLASSIGNMENT project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'KONANGUESSENND.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'KONANGUESSENND_URLASSIGNMENT.settings')
 
 application = get_wsgi_application()
